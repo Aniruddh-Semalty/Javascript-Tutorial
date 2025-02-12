99b9e336dd3a822ace4ea33ae875c92f7d3446c8
Aniruddh semalty <semaltyaniruddh001@gmail.com>
2025-02-12 06:43:08 +0000

    Add Math.js

---
+++ 06_nums_and_maths.js
@@ -0,0 +1,35 @@
+// const score=400
+
+// const balance=new Number(120);
+// console.log(balance) //number object
+
+// console.log(balance.toFixed(2)); //add decimal points
+// const newBalance=123.423;
+// console.log(newBalance.toFixed(2));
+// console.log(newBalance.toPrecision(2));
+// console.log(newBalance.toPrecision(3));
+// const bignumber=10000000000;
+// console.log(bignumber.toLocaleString());
+// console.log(bignumber.toLocaleString("en-IN")); 
+
+
+
+ //MATH
+//  console.log(Math);
+//  console.log(Math.round(4.3));
+//  console.log(Math.round(12.432));
+//  console.log(Math.ceil(4.2)); //5
+//  console.log(Math.floor(4.9)); //4
+//  console.log(Math.abs(-50));
+//  console.log(Math.min(4,3,10,23));
+//  console.log(Math.max(4,3,10,23));
+  
+
+  //Random
+  console.log(Math.random());
+  console.log(Math.random()*100); //between 0 to 100
+  console.log(Math.random()*100+1) ;//between 1 to 100;
+
+  const min=10
+  const max=20
+  console.log(Math.floor((Math.random()*(max-min+1)+min)));
